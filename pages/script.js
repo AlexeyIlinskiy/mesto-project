fe31f59160521f7142559282c2c4c13ce3b51970
Alexey Ilinskiy <alexey.ilinskiy@yandex.ru>
2022-04-04 00:15:48 +0300

    popup_edit_form

--- pages/script.js
+++ pages/script.js
@@ -24,12 +24,18 @@ const jobInput = document.querySelector('#form-edit-profile-about');
 function formSubmitHandler (evt) {
   evt.preventDefault();
   
-  nameInput.value;
-  jobInput.value;
+  let nameUser = document.querySelector('.profile__name');
+  let jobUser = document.querySelector('.profile__about');
 
+  nameUser.textContent = nameInput.value;
+  jobUser.textContent = jobInput.value;
+
+  console.log(nameInput.value);
+  formEditProfile.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
 
 //Откроем окно добавления карточки
 addBtnItem.onclick = function() {
@@ -40,3 +46,49 @@ addBtnItem.onclick = function() {
 closeBtnItem.onclick = function() {
   formAddItem.classList.remove('popup_opened');
 }
+
+//Данные для карточек: Название и ссылка на картику
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//Работаем с шаблоном создания карточки
+const galleryItems = document.querySelector('.gallery__items');
+const itemImg = initialCards.map
+
+function addItem (/*itemImg, itemTitle*/) {
+  const itemTemplate = document.querySelector('#item-template').content;
+  const itemElement = itemTemplate.querySelector('.item').cloneNode(true); 
+
+  itemElement.querySelector('.item__img').src = '';
+  itemElement.querySelector('.item__title').textContent = '';
+//  itemElement.querySelector('.item__img').src = itemImg;
+//  itemElement.querySelector('.item__title').textContent = itemTitle;
+
+  galleryItems.append(itemElement);
+}
+
+addItem();
