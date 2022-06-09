6a4bb2e9f97523dfa338581b17edfc19abc72c4a
Alexey Ilinskiy <alexey.ilinskiy@yandex.ru>
2022-06-09 17:56:29 +0300

    Пофиксил ошибки БЭМ

--- src/components/validate.js
+++ src/components/validate.js
@@ -8,7 +8,7 @@ const validParams = {
   inputSelector: '.form__input',
   submitButtonSelector: '.form__button',
   inactiveButtonClass: 'form__button_disabled',
-  inputErrorClass: 'form__input_type_error',
+  inputErrorClass: 'form__input_type-error',
   errorClass: 'form__input-error_active'
 };
 
