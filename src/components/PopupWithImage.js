6f07c6094087a85c4c8f67f40ac4a50c70f4b129
AlexeyIlinskiy <98812175+AlexeyIlinskiy@users.noreply.github.com>
2022-07-08 21:57:22 +0300

    Пофиксили не работающее закрытие на крестик

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,8 +1,8 @@
 import Popup from './Popup.js';
  
 export default class PopupWithImage extends Popup {
-  constructor (popupSelector, data) {
-    super(popupSelector);
+  constructor (popup, data) {
+    super(popup);
     this._image = data;
     this._imgBigSize = document.querySelector('.img-popup__img');
     this._imgPopupCaption = document.querySelector('.img-popup__caption');
